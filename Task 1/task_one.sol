fefc6d9b6aae1777571ef1920434cd0641e96bb3
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2022-09-23 18:27:59 +0530

    Task1

---
+++ Task 1/task_one.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.9;
+
+contract task_1{
+    
+    uint256 public num;
+
+    function write(uint256 n) public{
+        num = num1;
+    }
+
+    function read() public view returns(uint256){
+        return num;
+    }
+}
